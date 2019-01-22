cb2e030e4431f0e70dba30271b9be17935d8fa68
Baumgartner <sbaumg@gmail.com>
2019-01-22 09:33:07 +0100

    SEO update

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -17,6 +17,7 @@ export default class MyDocument extends Document {
           <meta name="msapplication-TileImage" content="/static/icons/ms-icon-144x144.png" />
           <meta name="theme-color" content="#2F2F2C" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <meta name="description" content="A workshop series with Vitaly Friedman. By ScriptConf. In Austria"/>
           <link rel="apple-touch-icon" sizes="57x57" href="/static/icons/apple-icon-57x57.png" />
           <link rel="apple-touch-icon" sizes="60x60" href="/static/icons/apple-icon-60x60.png" />
           <link rel="apple-touch-icon" sizes="72x72" href="/static/icons/apple-icon-72x72.png" />

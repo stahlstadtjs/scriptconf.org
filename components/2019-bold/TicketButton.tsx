4f5cfd5e1993f1f05bfa68e02460de521193b000
Baumgartner <sbaumg@gmail.com>
2019-05-21 16:04:27 +0200

    ticket

--- components/2019-bold/TicketButton.tsx
+++ components/2019-bold/TicketButton.tsx
@@ -1,7 +1,7 @@
 import "../../styles/TicketButton.css";
 
 export function TicketButton() {
-  return (<a href="/" data-ribbon="Tickets start EUR 129" title="Tickets start EUR 129" className="ticket-button" >
+  return (<a href="https://ti.to/scriptconf/scriptconf-2019" data-ribbon="Tickets start EUR 129" title="Tickets start EUR 129" className="ticket-button" >
     Tickets start EUR 129
   </a>);
 }

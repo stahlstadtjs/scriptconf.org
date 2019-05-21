5bb202702d43c490c6c0b45ecc0c09c101452b32
Baumgartner <sbaumg@gmail.com>
2019-05-21 16:03:46 +0200

    anim and focus

--- components/2019-bold/Speaker.tsx
+++ components/2019-bold/Speaker.tsx
@@ -6,12 +6,19 @@ type SpeakerProps = {
   selectedFn: (name: string) => void;
 };
 export function Speaker({ first, last, selectedFn }: SpeakerProps) {
-  return <h2 onMouseOut={() => selectedFn("")} onMouseOver={() => selectedFn(first)} className="speaker-block speaker">
-    <Link href={`/${first}`}>
-      <a>
-        <span className="drop">{first} </span>
-        <span className="nope">{last}</span>
-      </a>
-    </Link>
-  </h2>;
+  return (
+    <h2
+      onMouseOut={() => selectedFn("")}
+      onMouseOver={() => selectedFn(first)}
+      className="speaker-block speaker"
+    >
+      <Link href={`/${first}`}>
+        <a onFocus={() => selectedFn(first)}
+           onBlur={() => selectedFn("")}>
+          <span className="drop">{first} </span>
+          <span className="nope">{last}</span>
+        </a>
+      </Link>
+    </h2>
+  );
 }

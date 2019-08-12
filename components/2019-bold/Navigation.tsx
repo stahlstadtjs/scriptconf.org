3a85d7071d67c939dd6287bbf009f5471ea7336f
Baumgartner <sbaumg@gmail.com>
2019-08-12 09:11:46 +0200

    remove some data

--- components/2019-bold/Navigation.tsx
+++ components/2019-bold/Navigation.tsx
@@ -58,7 +58,7 @@ export function Navigation() {
     <Link href="/"><a><img className="nav-logo" src="/static/assets/caret.svg" alt="ScriptConf Home"/></a></Link>
     <a className="nav-link hide-sm" href="/#speakers">Speakers</a>
     <a className="nav-link hide-sm" href="/#workshops">Workshops</a>
-    <a className="nav-link hide-sm" href="/#jam">JAM Session</a>
+    {/* <a className="nav-link hide-sm" href="/#jam">JAM Session</a> */}
     <a className="nav-link hide-sm" href="/#venue">Venue</a>
     <a className="nav-button" href="https://ti.to/scriptconf/scriptconf-2019">Tickets @ 159 EUR</a>
   </nav>
--- components/2019-bold/speaker-data.ts
+++ components/2019-bold/speaker-data.ts
@@ -1,20 +1,20 @@
 export const speakerData = {
   entries: [
-    { first:"harry", last:"roberts", color:"green", myskills:"Performance consultant, Leeds, UK" },  
-    { first:"emma", last:"wedekind", color:"crimson", myskills:"Ladybug Podcast, JSParty.fm, Karlsruhe, Germany" },  
-    { first:"vitaly", last:"friedman", color:"hotpink", myskills:"Smashing Magazine, Worldwide" },  
-    { first:"jen", last:"looper", color:"#9013FE", myskills:"Microsoft Cloud Advocate, Boston, USA" },  
-    { first:"anna", last:"henningsen", color:"orange", myskills:"Node.js Core Team, Düsseldorf, Germany" },  
-    { first:"sara", last:"vieira", color:"dodgerblue", myskills:"Codesandbox, Berlin, Germany" },  
+    { first:"harry", last:"roberts", color:"green", myskills:"Performance consultant" },  
+    { first:"emma", last:"wedekind", color:"crimson", myskills:"Ladybug Podcast, JSParty.fm" },  
+    { first:"vitaly", last:"friedman", color:"hotpink", myskills:"Smashing Magazine" },  
+    { first:"jen", last:"looper", color:"#9013FE", myskills:"Microsoft Cloud Advocate" },  
+    { first:"anna", last:"henningsen", color:"orange", myskills:"Node.js Core Contributor" },  
+    { first:"sara", last:"vieira", color:"dodgerblue", myskills:"Codesandbox" },  
   ]
 }
 
 export const jamSessionData = {
   entries: [
     { first: "william", last: "imoh", color: "hotpink", myskills: "Scotch.io, Forloop Africa, JAMStack Lagos, Nigeria"},
-    { first: "vanessa", last: "böhner", color: "#9013fe", myskills: "Sinnerschrader, Storyblok Ambassador, Munich, Germany"},
-    { first: "alexandre", last: "chopin", color: "crimson", myskills: "Creator of Nuxt.js, Bordeaux, France"},
-    { first: "samuel", last: "snopko", color: "orange", myskills: "Storyblok Ambassador, Freiburg, Germany"},
-    { first: "matthew", last: "sweeney", color: "dodgerblue", myskills: "Zeit.co, UK"},
+    { first: "vanessa", last: "böhner", color: "#9013fe", myskills: "Sinnerschrader"},
+    { first: "alexandre", last: "chopin", color: "crimson", myskills: "Creator of Nuxt.js"},
+    { first: "samuel", last: "snopko", color: "orange", myskills: "Storyblok Ambassador"},
+    { first: "matthew", last: "sweeney", color: "dodgerblue", myskills: "Zeit.co"},
   ]
 }

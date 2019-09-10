bb6cb32f8a17e14c793415fac02d0dd658096686
Baumgartner <sbaumg@gmail.com>
2019-09-10 09:06:17 +0200

    add dylan

---
+++ pages/dylan.tsx
@@ -0,0 +1,14 @@
+import Content from '../content/2019/dylan.mdx';
+import { SpeakerDetail } from '../components/2019-bold/SpeakerDetail';
+import Head from "next/head";
+
+export default function() {
+  return <SpeakerDetail first="dylan" last="schiemann">
+    <Head>
+      {/*<meta property="og:image" content="https://scriptconf.org/static/assets/twittercard-harry.jpg" />
+      <meta name="twitter:image" content="https://scriptconf.org/static/assets/twittercard-harry.jpg" />*/}
+      <title>Dylan Schiemann | Script'19 - JavaScript in Linz</title>  
+    </Head>
+    <Content />
+  </SpeakerDetail>
+}

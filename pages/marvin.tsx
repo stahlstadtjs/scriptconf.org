3ca4e00e8c3a082a0da70da3dcab4471b4866547
Stefan Baumgartner <sbaumg@gmail.com>
2019-09-14 11:45:22 +0200

    add twitter card

--- pages/marvin.tsx
+++ pages/marvin.tsx
@@ -5,8 +5,8 @@ import Head from "next/head";
 export default function() {
   return <SpeakerDetail first="marvin" last="hagemeister">
     <Head>
-      {/*<meta property="og:image" content="https://scriptconf.org/static/assets/twittercard-harry.jpg" />
-      <meta name="twitter:image" content="https://scriptconf.org/static/assets/twittercard-harry.jpg" />*/}
+      <meta property="og:image" content="https://scriptconf.org/static/assets/twittercard-marvin.jpg" />
+      <meta name="twitter:image" content="https://scriptconf.org/static/assets/twittercard-marvin.jpg" />
       <title>Marvin Hagemeister | Script'19 - JavaScript in Linz</title>  
     </Head>
     <Marvin />

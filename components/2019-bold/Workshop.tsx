c5a260f7041d23a0b60ac570ef4e92e66a1759d6
Stefan Baumgartner <sbaumg@gmail.com>
2019-10-16 20:38:42 +0200

    ws

--- components/2019-bold/Workshop.tsx
+++ components/2019-bold/Workshop.tsx
@@ -48,14 +48,12 @@ export function Workshop({first, last, title, url}: WorkshopProps) {
       }
       
     `}</style>
-    <Link href={url}>
-      <a className="workshop-container">
-        <h3 className="tag">
-          <span className="block"><span className="highlight tag-line">{first} {last}</span></span>
-          <span className="block"><span className="tag-line">{title}</span></span>
-        </h3>
-        <img className="workshop-image" src={`/static/assets/${first}-workshop.jpg`} alt={`workshop by ${first} ${last}`}/>
-      </a>
-    </Link>
+    <a className="workshop-container" href={url}>
+      <h3 className="tag">
+        <span className="block"><span className="highlight tag-line">{first} {last}</span></span>
+        <span className="block"><span className="tag-line">{title}</span></span>
+      </h3>
+      <img className="workshop-image" src={`/static/assets/${first}-workshop.jpg`} alt={`workshop by ${first} ${last}`}/>
+    </a>
   </>
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -47,6 +47,13 @@ export default function() {
         <Workshop first="harry" last="roberts" title="Front-End Performance: Building Faster Websites" url="/harry/workshop-performance" />
         <Workshop first="vitaly" last="friedman" title="New Adventures in Front-End, 2019 Edition" url="/vitaly/workshop-front-end" />
       </div>
+
+      <section className="text about" id="workshops">
+        <h2>Community organised side events</h2>
+      </section>
+      <div className="workshop-wrapper">
+        <Workshop first="vue" last="vixens" title="Free Vue Vixens Workshop with Jen Looper" url="https://www.vuevixens.org/events/scriptcon19" />
+      </div>
       <section className="text about">
         <h2 id="jam">Oct 24: JAMstack JAM session</h2>
         <p>
